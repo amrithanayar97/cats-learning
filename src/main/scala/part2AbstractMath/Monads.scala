72286119c1fd6138fec766fe06cf79bae064da3c
amrithanayar97 <amritha.nayar@yonder.com>
2025-02-02 13:44:20 +0000

    learning 6

--- src/main/scala/part2AbstractMath/Monads.scala
+++ src/main/scala/part2AbstractMath/Monads.scala
@@ -1,5 +1,7 @@
 package part2AbstractMath
 
+import cats.implicits.toFlatMapOps
+
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -52,6 +54,8 @@ object Monads {
   trait MyMonad[M[_]] {
     def pure[A](value:A): M[A] // e.g if n is int -> List[Int]
     def flatMap[A,B](ma: M[A])(f: A => M[B]): M[B]
+    def map[A,B](ma: M[A])(f: A =>B): M[B] =
+      flatMap(ma)(x => pure(f(x))) // monads are also functors
   }
 
   // Cats Monad
@@ -87,10 +91,47 @@ object Monads {
 
   //powerful because we cna transform any monadic values
 
+  // extension methods - wierder imports - pure, flatMap
+  import cats.syntax.applicative._ //pure is here (part of applicative typeclass)
+  val oneOption = 1.pure[Option] // implicit Monad[Option] will be used => Some(1)
+  // can decorate any value and turn into a monadic value
+  val oneList = 1.pure[List] // List(1)
+  import cats.syntax.flatMap // flatMap is here part of its own package
+  // rn we have option, list and future already in scope all of which have their own implementation of flatMap so not very useful atm s o lets make our own monad
+  val oneOptionTransformed = oneOption.flatMap(x => (x + 1).pure[Option]) // flatMap becomes more useful when you implement data types that dont normally have their own flatMap method
+
+  //TO DO 3: implement map method in MyMonad -> line 55
+  // Monads extend functors so also have access to  functors extension methods
+  val oneOptionMapped = Monad[Option].map(Option(2))(_ + 1) // done explicitly here
+  import cats.syntax.functor._ //map is here
+  val oneOptionMapped2 = oneOption.map(_ +2)
+  // becuase we now have access to the map extension method and the flatMap extension method we can now have access to for comprehnesions
+  val composedOptionFor = for {
+    one <- 1.pure[Option]
+    two <- 2.pure[Option]
+  } yield one + two
+
+  //TO DO 4: implement a shorter version of getPairs using for-comp
+  def getPairs2[M[_], A, B](ma: M[A], mb: M[B])(implicit monad: Monad[M]): M[(A,B)] =
+    for {
+      a <- ma
+      b <- mb
+    } yield (a,b) // same as ma.flatMap(a => mb.map(b => (a,b)))
+
+
+  // SUMMARY
+  // Monads are a higher-kinded type class that provides 2 fundamenal methods
+  // - a pure method to wrap a normal value into a monadic value
+  // - a flatMap method to transform monadic values in sequence
+  // can implement map in terms of pure and flatMap, monads are natual extension of functors
+  // extension methods are in other packages
+  // monads are a powerful took for sequential transformations e.g: list combinations, option combinations, asynchronous chained computations and dependant computations
+  // FOR_COMPREHENSIONS ARE NOT ITERERATIONS, more a mental model of chained transformations
+
   def main(args: Array[String]): Unit = {
     print(all)
-    print(getPairs(numbersList,charsList)) // suddenly okay since monad lisy in scope
-    print(getPairs(numberOption,charOption))
+    print(getPairs2(numbersList,charsList)) // suddenly okay since monad list in scope
+    print(getPairs2(numberOption,charOption))
     getPairs(numberFuture,charFuture).foreach(print) // same as when run earlier
   }
 }
